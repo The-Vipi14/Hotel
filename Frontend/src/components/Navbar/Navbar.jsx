e73004726272fae2c46c04fd03b83a273fb9e631
Vipin <vipinbhardwal286@gmail.com>
2026-01-18 00:01:17 +0530

    Added hotel navbar layout

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Radiant Plaza</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Rooms</li>
+        <li>Restaurant</li>
+        <li>Banquet</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+      </ul>
+
+      <button className="book-btn">Book Now</button>
+    </nav>
+  )
+}
+
+export default Navbar
