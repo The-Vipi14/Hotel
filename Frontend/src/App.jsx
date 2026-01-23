7f1f45bf774db913091bbfa408f57af9d043bffd
Vipin <vipinbhardwal286@gmail.com>
2026-01-23 23:50:08 +0530

    Lazy loading with loading component:

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,38 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import Banquet from "./pages/banquet/Banquet";
-import Contact from "./pages/contact/Context";
-import Gallery from "./pages/gallery/Gallery";
-import Home from "./pages/Home/Home";
-import Restaurant from "./pages/Restaurant/Restaurant";
-import Rooms from "./pages/rooms/Rooms";
-import Reservation from "./pages/reservation/Reservation";
+import Loading from "./components/loading/Loading";
+
+const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+const Footer = lazy(() => import("./components/footer/Footer"));
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Banquet = lazy(() => import("./pages/banquet/Banquet"));
+const Contact = lazy(() => import("./pages/contact/Context"));
+const Gallery = lazy(() => import("./pages/gallery/Gallery"));
+const Restaurant = lazy(() => import("./pages/Restaurant/Restaurant"));
+const Rooms = lazy(() => import("./pages/rooms/Rooms"));
+const Reservation = lazy(() => import("./pages/reservation/Reservation"));
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      <Suspense fallback={<Loading/>}>
+        <Navbar />
 
-      <main className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<Rooms />} />
-          <Route path="/restaurant" element={<Restaurant />} />
-          <Route path="/banquet" element={<Banquet />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/reservation" element={<Reservation/>} />
-        </Routes>
-      </main>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/restaurant" element={<Restaurant />} />
+            <Route path="/banquet" element={<Banquet />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/reservation" element={<Reservation />} />
+          </Routes>
+        </main>
 
-      <Footer />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
