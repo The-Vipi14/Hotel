23e2f745a82c74f073f50d32dab29785b9fd60e3
Vipin <vipinbhardwal286@gmail.com>
2026-02-01 13:16:24 +0530

    logout route added

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { adminLogin, adminRegister } = require("../controllers/admin.controller")
+const { adminLogin, adminRegister,adminLogout } = require("../controllers/admin.controller")
 
 const router = express.Router()
 
 router.post("/login", adminLogin)
 router.post("/register", adminRegister)
+router.post("/logout", adminLogout)
 
 module.exports = router
